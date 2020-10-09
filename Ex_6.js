a1bf778540f65bd21d4792cae108124dbb9c2413
raperock <72583135+raperock@users.noreply.github.com>
2020-10-09 11:59:50 +0300

    Update Ex_6.js

--- Ex_6.js
+++ Ex_6.js
@@ -1,7 +1,12 @@
-let arr = [1,1,3,1];
-for (let i=0; i<arr.length; i++){
-    if arr[i]==arr[i+1]{
-        alert()
+let arr=[1,1,1,1,1,1,1];
+let a=arr[0];
 
+let ifs=false;
+
+for (let i=0; i<arr.length; i++){
+    if (a===arr[i]){
+        ifs=true
     }
+    else ifs=false;
 }
+console.log(ifs);
