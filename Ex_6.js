fce31f5f9a64d7a68cda2a20e422bcab2452c506
raperock <72583135+raperock@users.noreply.github.com>
2020-10-09 14:12:30 +0300

    Update Ex_6.js

--- Ex_6.js
+++ Ex_6.js
@@ -1,10 +1,10 @@
-let arr=[1,1,1,1,1,1,1];
+let arr=[1,1,'9',1,1,1,1,1];
 let a=arr[0];
 
 let ifs=false;
 
 for (let i=0; i<arr.length; i++){
-    if (a===arr[i]){
+    if (a===arr[i+1]){
         ifs=true
     }
     else ifs=false;
